006e408eb5634ce12a18c08c2bf25e84d4d268d0
ayoeuic <ayoe@uic.edu>
2026-04-23 15:55:54 -0500

    changing arrow ui

--- GravLift/Assets/Scripts/ARROW.cs
+++ GravLift/Assets/Scripts/ARROW.cs
@@ -3,16 +3,20 @@
  * Description: Script that manages the arrow UI element
  */
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ARROW : MonoBehaviour {
     [SerializeField] private GRAVITY gravity;
+    private Image arrow;
     private RectTransform rect_transform;
 
     private void Awake() {
+        arrow = GetComponent<Image>();
         rect_transform = GetComponent<RectTransform>();
     }
 
     private void Update() {
         rect_transform.localRotation = Quaternion.Euler(0.0f, 0.0f, gravity.IsUpsideDown() ? 90.0f : -90.0f);
+        arrow.color = gravity.IsUpsideDown() ? Color.red : Color.blue;
     }
 }
